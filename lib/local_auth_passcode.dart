f166c124b4d603138ea25ecd9bd90c8d6f863997
gmcdowell <greg@oxigeno.co.nz>
2019-01-18 13:13:08 +1300

    refactors to use generators for focus nodes, text edit ctlrs and input elements

--- lib/local_auth_passcode.dart
+++ lib/local_auth_passcode.dart
@@ -8,27 +8,21 @@ import 'package:flutter/material.dart';
 class PasscodeAuth extends StatefulWidget {
   PasscodeAuth({
     Key key,
+    this.inputLength = 4,
     // @required this.onSubmit,
   }) : super(key: key);
 
   // final Function onSubmit;
+  final int inputLength;
 
   @override
   _PasscodeAuthState createState() => _PasscodeAuthState();
 }
 
 class _PasscodeAuthState extends State<PasscodeAuth> {
-  FocusNode _input1Focus;
-  FocusNode _input2Focus;
-  FocusNode _input3Focus;
-  FocusNode _input4Focus;
+  List<FocusNode> _focusNodes;
 
-  TextEditingController _input1Ctl;
-  TextEditingController _input2Ctl;
-  TextEditingController _input3Ctl;
-  TextEditingController _input4Ctl;
-
-  // List<String> _passcode = List<String>();
+  List<TextEditingController> _txtCtlrs;
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
@@ -36,63 +30,62 @@ class _PasscodeAuthState extends State<PasscodeAuth> {
   void initState() {
     super.initState();
 
-    _input1Focus = FocusNode();
-    _input2Focus = FocusNode();
-    _input3Focus = FocusNode();
-    _input4Focus = FocusNode();
+    _buildFocusNodes();
 
-    _input1Ctl = TextEditingController();
-    _input2Ctl = TextEditingController();
-    _input3Ctl = TextEditingController();
-    _input4Ctl = TextEditingController();
-
-    _input1Ctl.addListener(_onChange);
-    _input2Ctl.addListener(_onChange);
-    _input3Ctl.addListener(_onChange);
-    _input4Ctl.addListener(_onChange);
+    _buildTextEditingControllers(listener: _onChange);
   }
 
   @override
   void dispose() {
-    _input1Ctl.dispose();
-    _input2Ctl.dispose();
-    _input3Ctl.dispose();
-    _input4Ctl.dispose();
+    // disponse text editing controllers
+    _txtCtlrs.forEach((TextEditingController ctlr) => ctlr.dispose());
 
-    _input1Focus.dispose();
-    _input2Focus.dispose();
-    _input3Focus.dispose();
-    _input4Focus.dispose();
+    // dispose focus nodes
+    _focusNodes.forEach((FocusNode node) => node.dispose());
 
     super.dispose();
   }
 
+  void _buildFocusNodes() {
+    _focusNodes = List.generate(widget.inputLength, (_) => FocusNode());
+  }
+
+  void _buildTextEditingControllers({Function listener}) {
+    _txtCtlrs =
+        List.generate(widget.inputLength, (_) => TextEditingController());
+
+    if (listener != null) {
+      _txtCtlrs
+          .forEach((TextEditingController ctlr) => ctlr.addListener(listener));
+    }
+  }
+
   String get pin =>
-      '${_input1Ctl.text}${_input2Ctl.text}${_input3Ctl.text}${_input4Ctl.text}';
+      _txtCtlrs.map((TextEditingController ctlr) => ctlr.text).toList().join();
 
   void _onChange() {
-    //   // if the value entered is 1 digit in length, then move focus to next input
-    if (_input1Focus.hasFocus) {
-      if (_input1Ctl.text.length == 1) {
-        _input1Focus.unfocus();
-        FocusScope.of(_formKey.currentContext).requestFocus(_input2Focus);
-      }
-    } else if (_input2Focus.hasFocus) {
-      if (_input2Ctl.text.length == 1) {
-        _input2Focus.unfocus();
-        FocusScope.of(_formKey.currentContext).requestFocus(_input3Focus);
-      }
-    } else if (_input3Focus.hasFocus) {
-      if (_input3Ctl.text.length == 1) {
-        _input3Focus.unfocus();
-        FocusScope.of(_formKey.currentContext).requestFocus(_input4Focus);
-      }
-    } else if (_input4Focus.hasFocus) {
-      if (_input4Ctl.text.length == 1) {
+    int index;
+    TextEditingController ctlr;
+
+    FocusNode focusNodeInFocus = _focusNodes
+        .firstWhere((FocusNode node) => node.hasFocus, orElse: () => null);
+
+    if (focusNodeInFocus != null) {
+      index = _focusNodes.indexOf(focusNodeInFocus);
+      ctlr = _txtCtlrs.elementAt(index);
+    }
+
+    if (ctlr != null && ctlr.text.length == 1) {
+      // set focus on next node
+      int nextIndex = index == widget.inputLength - 1 ? 0 : index + 1;
+      FocusNode nextFocusNode = _focusNodes.elementAt(nextIndex);
+      FocusScope.of(_formKey.currentContext).requestFocus(nextFocusNode);
+
+      // if on last elements, reset the form
+      if (index == widget.inputLength - 1) {
         print('call submit from onChange() $pin');
 
         _formKey.currentState.reset();
-        FocusScope.of(_formKey.currentContext).requestFocus(_input1Focus);
       }
     }
   }
@@ -105,6 +98,52 @@ class _PasscodeAuthState extends State<PasscodeAuth> {
     ),
   );
 
+  Widget _buildInputField({
+    BuildContext context,
+    bool autofocus = false,
+    Function onFieldSubmitted,
+    FocusNode focusNode,
+    TextEditingController textEditingController,
+  }) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.only(left: 8.0, right: 8.0),
+        child: TextFormField(
+          enabled: false,
+          keyboardType: TextInputType.number,
+          textInputAction: TextInputAction.none,
+          focusNode: focusNode,
+          autofocus: autofocus,
+          controller: textEditingController,
+          onFieldSubmitted: (_) {
+            if (textEditingController.text.length == 1) {
+              onFieldSubmitted.call();
+            }
+          },
+          obscureText: true,
+          maxLength: 1,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: Theme.of(context).textTheme.display1.fontSize,
+            color: Theme.of(context).textTheme.display1.color,
+          ),
+          enableInteractiveSelection: false,
+          decoration: _decoration,
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildRowChildren(BuildContext context) => List<Widget>.generate(
+      widget.inputLength,
+      (int index) => _buildInputField(
+            context: context,
+            autofocus: index == 0 ? true : false,
+            onFieldSubmitted: () => _onChange,
+            focusNode: _focusNodes.elementAt(index),
+            textEditingController: _txtCtlrs.elementAt(index),
+          ));
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -115,146 +154,7 @@ class _PasscodeAuthState extends State<PasscodeAuth> {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           verticalDirection: VerticalDirection.down,
-          children: <Widget>[
-            // input 1
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(left: 8.0, right: 8.0),
-                child: TextFormField(
-                  enabled: false,
-                  keyboardType: TextInputType.number,
-                  textInputAction: TextInputAction.next,
-                  focusNode: _input1Focus,
-                  autofocus: true,
-                  controller: _input1Ctl,
-                  // onSaved: (val) {
-                  //   _passcode[0] = val;
-                  // },
-                  onFieldSubmitted: (val) {
-                    if (_input1Ctl.text.length == 1) {
-                      // _input1Focus.unfocus();
-                      // FocusScope.of(context).requestFocus(_input2Focus);
-                      _onChange();
-                    }
-                  },
-                  obscureText: true,
-                  maxLength: 1,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    color: Theme.of(context).textTheme.body1.color,
-                  ),
-                  enableInteractiveSelection: false,
-                  decoration: _decoration,
-                ),
-              ),
-            ),
-
-            // // input 2
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(left: 8.0, right: 8.0),
-                child: TextFormField(
-                  enabled: false,
-                  keyboardType: TextInputType.number,
-                  textInputAction: TextInputAction.next,
-                  focusNode: _input2Focus,
-                  controller: _input2Ctl,
-                  obscureText: true,
-                  // onSaved: (val) {
-                  //   _passcode[1] = val;
-                  // },
-                  onFieldSubmitted: (val) {
-                    if (_input2Ctl.text.length == 1) {
-                      // _input2Focus.unfocus();
-                      // FocusScope.of(context).requestFocus(_input3Focus);
-                      _onChange();
-                    }
-                  },
-                  maxLength: 1,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    color: Theme.of(context).textTheme.body1.color,
-                  ),
-                  enableInteractiveSelection: false,
-                  decoration: _decoration,
-                ),
-              ),
-            ),
-
-            // // input 3
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(left: 8.0, right: 8.0),
-                child: TextFormField(
-                  enabled: false,
-                  keyboardType: TextInputType.number,
-                  textInputAction: TextInputAction.next,
-                  focusNode: _input3Focus,
-                  controller: _input3Ctl,
-                  obscureText: true,
-                  // onSaved: (val) {
-                  //   _passcode[2] = val;
-                  // },
-                  onFieldSubmitted: (val) {
-                    if (_input3Ctl.text.length == 1) {
-                      // _input3Focus.unfocus();
-                      // FocusScope.of(context).requestFocus(_input4Focus);
-                      _onChange();
-                    }
-                  },
-                  maxLength: 1,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    color: Theme.of(context).textTheme.body1.color,
-                  ),
-                  enableInteractiveSelection: false,
-                  decoration: _decoration,
-                ),
-              ),
-            ),
-
-            // // input 4
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(left: 8.0, right: 8.0),
-                child: TextFormField(
-                  enabled: false,
-                  keyboardType: TextInputType.number,
-                  textInputAction: TextInputAction.done,
-                  focusNode: _input4Focus,
-                  controller: _input4Ctl,
-                  obscureText: true,
-                  // onSaved: (val) {
-                  //   _passcode[3] = val;
-                  // },
-                  // onFieldSubmitted: (val) {
-                  //   _input4Focus.unfocus();
-                  //   print('call submit');
-                  //   FocusScope.of(context).requestFocus(_input1Focus);
-                  // },
-                  onEditingComplete: () {
-                    print('onEditingComplete heard');
-                    if (_input4Ctl.text.length == 1) {
-                      // _input4Focus.unfocus();
-                      // FocusScope.of(context).requestFocus(_input1Focus);
-                      _onChange();
-                    }
-                  },
-                  maxLength: 1,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    color: Theme.of(context).textTheme.body1.color,
-                  ),
-                  enableInteractiveSelection: false,
-                  decoration: _decoration,
-                ),
-              ),
-            ),
-          ],
+          children: _buildRowChildren(context),
         ),
       ),
     );
